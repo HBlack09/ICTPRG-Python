fbfbe000b2f0e3ea17d37fff4af38aaa319c65d9
HBlack09 <72083420+HBlack09@users.noreply.github.com>
2021-06-02 11:12:04 +1000

    Add files via upload

---
+++ Debugging/Question 1.py
@@ -0,0 +1,5 @@
+greeting = input("Hello, possible pirate! What's the password?")
+if greeting in ["Arrr!"]:
+    print("Go away, pirate.")
+elif greeting != ("Arrr!"):
+    print("Greetings, hater of pirates!")
