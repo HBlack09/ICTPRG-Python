53057ba10219901a0c690acb8c8cbdf911f71cc1
HBlack09 <72083420+HBlack09@users.noreply.github.com>
2021-06-23 11:56:06 +1000

    Update Question 2.py

--- Arrays and Lists/Question 2.py
+++ Arrays and Lists/Question 2.py
@@ -5,6 +5,6 @@ date = input("Enter a date in the format dd/mm/yyyy: ")
 dateList = date.split("/")
 
 #Printing the date 
-print(f"Date: {dateList[0]}")
-print(f"Date: {dateList[1]}")
-print(f"Date: {dateList[2]}")
+print(f"Day: {dateList[0]}")
+print(f"Month: {dateList[1]}")
+print(f"Year: {dateList[2]}")
