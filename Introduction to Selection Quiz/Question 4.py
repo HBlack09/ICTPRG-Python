327180ac068473afa5bd400ae376ddaec85bb2b7
HBlack09 <72083420+HBlack09@users.noreply.github.com>
2021-06-23 11:08:45 +1000

    Update Question 4.py

--- Introduction to Selection Quiz/Question 4.py
+++ Introduction to Selection Quiz/Question 4.py
@@ -1,6 +1,8 @@
+#Ask user to input to input username and password
 username = input("Enter your username: ")
 password = input("Enter your password: ")
 
+#Define what the correct username and password are + grant/deny access
 if username == "bob" and password == "password1234":
          print("Access Granted!")
 elif username == "fred" and password == "happyPass122": 
